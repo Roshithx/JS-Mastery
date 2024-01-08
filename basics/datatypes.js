6644501a06d80e0de986ddb2d073360728ee5228
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-01-08 20:15:39 +0530

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) //not working we use nodejs,not browser
+
+console.log(3 +2); 
+
+let name="Roshi"
+let age=12
+let isLogin= false
+
+
+//number
+//bigint
+//string =>""
+//boolean => true/false
+// null => standalone value  type="object"
+//undefined=>
+//symbol => unique things
+
+
+//object
+
+ console.log(typeof age );
