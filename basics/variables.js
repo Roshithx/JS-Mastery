cded7e05dcd22dc8470b56becdbe14f0108ac628
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-01-08 20:18:25 +0530

    variable.js

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+ const accountId=144553
+ let accountemail="roshi@gmail.com"
+ var accountpass="12345"//Not used in modern js  because of issue in block scope and functional scope
+ accountCity="Kannur"
+let accountstate;
+//  accountId=2 const cant be changed
+accountemail="dfkghjdf@gmail.com"
+accountpass="4543"
+accountCity="abc"
+ console.log(accountId); 
+ console.table([accountId,accountemail,accountpass,accountCity,accountstate])
