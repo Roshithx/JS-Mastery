b11c96495417001d27855127ca650c19679e4dcf
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-01-08 20:37:20 +0530

    conversion of dt

---
+++ basics/conversionOperation.js
@@ -0,0 +1,20 @@
+let score="afdf"
+
+//console.log(typeof score);
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+// TYPE CONVERSION
+// "33"= 33
+// "hi" = Nan
+let booleanis = 3
+let con=Boolean(booleanis)
+console.log(con);
+
+
+let no=44
+let st=String(no)
+console.log(st);
+console.log(typeof st);
