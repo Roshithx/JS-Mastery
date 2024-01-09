6bbc90dcde94a1ccf81701c7aaea17eb652bc4c3
Roshith P <125194847+Roshithx@users.noreply.github.com>
2024-01-09 19:00:22 +0530

    conversion

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -11,10 +11,36 @@ let score="afdf"
 // "hi" = Nan
 let booleanis = 3
 let con=Boolean(booleanis)
-console.log(con);
+//console.log(con);
 
 
 let no=44
 let st=String(no)
-console.log(st);
-console.log(typeof st);
+//console.log(st);
+//console.log(typeof st);
+
+//************* OPERATIONS***************/
+ 
+let value=3
+let negvalue= -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**4);
+
+let str="helo"
+let str1="rosho"
+
+let str2=str+str1
+// console.log(str2);
+
+// console.log("2"+1);
+// console.log("1"+2);
+// console.log("2"+2 +2)
+// console.log(1 + 2 +"2");
+
+let counter=100
+++counter;
+console.log(counter);
+
